006b66d3e2051ae517d0bb2215a1dc33a9db6d29
pandeyhitesh <hiteshpandey206@gmail.com>
2021-04-01 13:17:50 +0530

    new migration and WHITENOISE_AUTOREFRESH turned on

--- connectMysql/settings.py
+++ connectMysql/settings.py
@@ -104,6 +104,7 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
